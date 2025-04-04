0e840a5c4fe20399a58ba2ff483e476cfcff598e
jihye <wisdom4466@naver.com>
2025-04-04 15:29:49 +0900

    초기 프로젝트 세팅 완료 (Hello, Hatimo!)

---
+++ src/main/java/com/hatimo/heartemotion/HatimoApplication.java
@@ -0,0 +1,13 @@
+package com.hatimo.heartemotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hatimo/heartemotion/HatimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatimo.heartemotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
