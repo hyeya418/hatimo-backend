dd1669d152b51bbb7cd5ca2618225c0fad4168e5
jihye <wisdom4466@naver.com>
2025-04-29 13:42:22 +0900

    CHORE: dotenv 환경변수 관리

--- src/main/java/com/hatimo/heartemotion/HatimoApplication.java
+++ src/main/java/com/hatimo/heartemotion/HatimoApplication.java
@@ -1,5 +1,6 @@
 package com.hatimo.heartemotion;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HatimoApplication {
 
 	public static void main(String[] args) {
+		// .env 파일 로딩
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./") // 루트 경로
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("KAKAO_REST_API_KEY", dotenv.get("KAKAO_REST_API_KEY"));
+		System.setProperty("KAKAO_REDIRECT_URI", dotenv.get("KAKAO_REDIRECT_URI"));
+
 		SpringApplication.run(HatimoApplication.class, args);
 	}
 
